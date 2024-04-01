7c728914dd033d27c4cda91a46e40bba6575a1a0
Deric Colander <dcolanderjr@gmail.com>
2024-04-01 17:40:49 -0400

    update

--- Terraform-VPC/modules/ec2/compute.tf
+++ Terraform-VPC/modules/ec2/compute.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
   subnet_id                     = var.subnet_id[count.index]
   associate_public_ip_address   = var.associate_public_ip_address
   vpc_security_group_ids        = [var.security_group_id]
-  user_data                     = file("${path.module}/user_data.sh")
+  #user_data                     = file("${path.module}/user_data.sh")
   
   tags = {
     Name = var.instance_name
