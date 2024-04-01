06de71f22a919c0d59d8b6f69f740d584fc59336
Deric Colander <dcolanderjr@gmail.com>
2024-04-01 13:58:14 -0400

    update

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -74,19 +74,3 @@ module "loadbalancer" {
 
   depends_on = [module.vpc, module.security_group, module.ec2]
 }
-
-output "alb_dns_name" {
-  value = module.loadbalancer.alb_dns_name
-}
-
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
-
-output "ec2_instance_id" {
-  value = module.ec2.instance_id
-}
-
-output "ec2_instance_tags" {
-  value = module.ec2.instance_tags
-}
