e1cf8160e05a9627bce8eba8c0f076c87ad3f2af
dC <131455625+dcolanderjr@users.noreply.github.com>
2024-04-02 20:53:42 -0400

    Update variables.tf

--- Terraform-VPC/modules/ecr/variables.tf
+++ Terraform-VPC/modules/ecr/variables.tf
@@ -139,3 +139,8 @@ variable "github_assume_role_policy" {
   type        = string
   description = "Assume role policy for GitHub Actions"
 }
+
+variable "create_ecr_repository" {
+  description = "Controls whether to create the ECR repository"
+  type        = bool
+}
